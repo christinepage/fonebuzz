435b5f9a47f5314b6424da636086681a29abde71
Christine Page <sigworld@gmail.com>
2014-07-10 21:28:08 -0700

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monkey::Application.config.secret_key_base = 'cdc5a78c18931d1ceec03bafd02dbddb262bb9d576d2c9a7c889be416692f48c443590fcc4c74b011befa5f3e3987344ca5e29c029e87802a1bfb47b38a65939'
