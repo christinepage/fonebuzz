006d925a131fee5a3eca7e6eaed02b5d040c55be
Christine Page <sigworld@gmail.com>
2014-07-11 22:18:14 -0700

    revert log to stdout

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Monkey::Application.initialize!
 
-Rails.logger = Logger.new(STDOUT)
+#Rails.logger = Logger.new(STDOUT)
