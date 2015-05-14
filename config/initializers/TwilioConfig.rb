1db12d5a657fc498b0a187a63036b295f7c3d968
Christine Page <sigworld@gmail.com>
2015-05-14 11:43:12 -0700

    tabs->spaces

--- config/initializers/TwilioConfig.rb
+++ config/initializers/TwilioConfig.rb
@@ -1,20 +1,20 @@
 module TwilioConfig
 extend self
-	def config_file
-	  "#{Rails.root.to_s}/config/twilio_config.yml"
-	end
+  def config_file
+    "#{Rails.root.to_s}/config/twilio_config.yml"
+  end
 
-	def config_hash
-	  if @config_hash.nil?
+  def config_hash
+    if @config_hash.nil?
       puts "loading Twilio config..."
       @config_hash = YAML.load(ERB.new(File.read("#{config_file}")).result) || {}
     end
     @config_hash
-	end
+  end
 
-	def config_param(key)
-	  config_hash[key]
-	end
+  def config_param(key)
+    config_hash[key]
+  end
 
 end
 
